fdbf4eda7aeae6a91eeaad84c90cd16e6ae1e62e
Kim HyunWoo <stories2@naver.com>
2018-08-01 09:27:52 +0900

    home.component.ts: inline template, styles

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  // templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  template: `
+    <p>home works!</p>
+  `,
+  styles: [
+    `p {
+      color: red;
+    }`
+  ]
 })
 export class HomeComponent implements OnInit {
 
