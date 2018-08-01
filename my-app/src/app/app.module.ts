5f23ef9be87e93a1008ebb7a3bc457ddd84c7114
Kim HyunWoo <stories2@naver.com>
2018-08-01 09:29:59 +0900

    highlight.directive.ts : make new directive

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
