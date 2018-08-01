c99a62c658dfc1a39b9a40b86e7962f69edc0344
Kim HyunWoo <stories2@naver.com>
2018-08-01 13:29:48 +0900

    structure-directive.component: built in directive ngFor

--- hello/src/app/structure-directive/structure-directive.component.ts
+++ hello/src/app/structure-directive/structure-directive.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Customer {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-structure-directive',
   templateUrl: './structure-directive.component.html',
@@ -8,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
 export class StructureDirectiveComponent implements OnInit {
 
   isShow = true;
+  customerList: Customer[] = [
+    {id: 1, name: "Customer 1"},
+    {id: 2, name: "Customer 2"},
+    {id: 3, name: "Customer 3"},
+
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addCustomer(customerName: string) {
+    if(customerName) {
+      this.customerList.push({id: this.getNewCustomerID(), name: customerName});
+    }
+  }
+
+  removeUser(userID: number) {
+    this.customerList = this.customerList.filter(({id}) => id !== userID);
+  }
+
+  getNewCustomerID(): number {
+    return this.customerList.length ? Math.max(...this.customerList.map(({id}) => id)) + 1 : 1;
+  }
 }
