d96c818ce40cd7c040bf2a2b4c86934b1f3eb251
Kim HyunWoo <stories2@naver.com>
2018-08-01 14:40:45 +0900

    user-list.component: show child user list as table

--- component-interaction/src/app/app.component.ts
+++ component-interaction/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "./models/user.model";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'component-interaction';
+  userList: User[];
+
+  constructor() {
+    this.userList = [
+      {id: 1, name: "Lee", role: "Admin"},
+      {id: 2, name: "Baek", role: "Developer"},
+      {id: 3, name: "Park", role: "Designer"},
+    ];
+  }
+
+  addUser(name: string, role: string) {
+    if(name && role) {
+      this.userList = [...this.userList,
+        {id: this.getNextID(), name: name, role: role}
+      ]
+    }
+  }
+
+  getNextID(): number {
+    return this.userList.length ? Math.max(...this.userList.map(({ id }) => id)) + 1 : 1;
+  }
 }
--- component-interaction/src/app/user-list/user-list.component.ts
+++ component-interaction/src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {User} from "../models/user.model";
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+
+  @Input() childUserList: User;
 
   constructor() { }
 
